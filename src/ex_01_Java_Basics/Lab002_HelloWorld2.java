ea879a0bcd55b64aabd82833e514e102d0a8bc73
shraddhanair-25 <shraddhanair2010@gmail.com>
2025-12-21 21:01:25 +0530

    My first Java code HelloWorld2

---
+++ src/ex_01_Java_Basics/Lab002_HelloWorld2.java
@@ -0,0 +1,19 @@
+package ex_01_Java_Basics;
+
+import java.security.spec.RSAOtherPrimeInfo;
+
+public class Lab002_HelloWorld2 {
+    public static void main(String[] args){
+        System.out.println("Hello");
+
+        /*if we change public to private JVM will not recognise
+        JVM only recognise the signature
+        public static void main(String[])
+        it will only recognise in higher version 25 ++
+
+
+          */
+
+
+    }
+}
